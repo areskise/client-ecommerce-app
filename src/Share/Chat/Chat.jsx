66596c55b414dd9aff408230c17063447f36b890
Ares Kise <areskise@gmail.com>
2023-01-01 23:28:46 +0700

    fix chat

--- src/Share/Chat/Chat.jsx
+++ src/Share/Chat/Chat.jsx
@@ -88,10 +88,10 @@ function Chat(props) {
 					//Tiếp theo nó sẽ postdata lên api đưa dữ liệu vào database
 					ChatRoomsAPI.addMessage(data);
 					setTextMessage('');
-					setTimeout(() => {
-						// socket.emit('send_message', data);
-						// channel.trigger("send_message", data);
-					}, 200);
+					// setTimeout(() => {
+					// 	// socket.emit('send_message', data);
+					// 	// channel.trigger("send_message", data);
+					// }, 200);
 				})
 				.catch(err => console.log(err))
 		} else {
@@ -105,10 +105,10 @@ function Chat(props) {
 			ChatRoomsAPI.addMessage(data);
 			setTextMessage('');
 			
-			setTimeout(() => {
-				// socket.emit('send_message', data);
-				// channel.trigger("send_message", data);
-			}, 200);
+			// setTimeout(() => {
+			// 	// socket.emit('send_message', data);
+			// 	// channel.trigger("send_message", data);
+			// }, 200);
 		}
 		setLoad(true)
 	};
